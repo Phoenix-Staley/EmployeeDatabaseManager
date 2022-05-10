c83afdf3508108fb63b341b7a6a7eaffb38eabfc
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-10 09:46:45 -0700

    Attempt to throw err if db doesn't exist

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+// const schema = require("../db/schema.sql");
+// console.log(schema);
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: "password1",
-    database: 'test'
+    host: "localhost",
+    user: "root",
+    password: "password1"
+});
+connection.query('SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = "company_db"', (err, results) => {
+    if (!results[0]) {
+        throw "It seems you do not have a database set up. Please run schema.sql and seeds.sql";
+    } else {
+        connection.query("USE company_db");
+    }
 });
 const mainOptions = [
     {
@@ -33,26 +41,19 @@ connection.connect();
 //         USE company
 //     END`);
 
+console.log(String.raw`
+ ______                 _                          _____  ____     __  __                                   
+|  ____|               | |                        |  __ \|  _ \   |  \/  |                                  
+| |__   _ __ ___  _ __ | | ___  _   _  ___  ___   | |  | | |_) |  | \  / | __ _ _ __   __ _  __ _  ___ _ __ 
+|  __| | '_ \ _ \| '_ \| |/ _ \| | | |/ _ \/ _ \  | |  | |  _ <   | |\/| |/ _\ | '_ \ / _\ |/ _\ |/ _ | '__|
+| |____| | | | | | |_) | | (_) | |_| |  __|  __/  | |__| | |_) |  | |  | | (_| | | | | (_| | (_| |  __| |   
+|______|_| |_| |_| .__/|_|\___/ \__, |\___|\___|  |_____/|____/   |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|   
+                 | |             __/ |                                                       __/ |          
+                 |_|            |___/                                                       |___/           
+`);
+
 inquirer.prompt(mainOptions)
 .then((choice) => {
-    connection.query('SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = "company"', (err, results) => {
-        // console.log(results);
-        if (!results[0]) {
-            connection.execute('CREATE DATABASE company');
-            connection.execute('USE company');
-            (() => {
-                connection.execute(`CREATE TABLE employees (id INT NOT NULL AUTO_INCREMENT,
-                    firstName VARCHAR(31) NOT NULL,
-                    lastName VARCHAR(31) NOT NULL,
-                    roleId INT NOT NULL,
-                    managerId INT,
-                    PRIMARY KEY (id),
-                    FOREIGN KEY (roleId) REFERENCES roles(id))`);
-            })
-            console.log("New 'company' database made!");
-        } else {
-            console.log("Results != [] :", results);
-        }
+        console.log(choice);
         connection.end();
     });
-});
