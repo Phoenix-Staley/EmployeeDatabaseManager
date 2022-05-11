d4df7ddd517116d19948bc957abfa233b97b39e1
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-11 14:30:07 -0700

    Move 'continue?' inquiry to a function

--- helpers/quieries.js
+++ helpers/quieries.js
@@ -1,10 +1,30 @@
+const inquirer = require("inquirer");
+
+const wait_to_resolve = (resolve) => {
+    inquirer.prompt([{
+        type: "list",
+        choices: ["Ok"],
+        message: "Press 'Ok' to continue",
+        name: "continue"
+    }])
+    .then(res => resolve());
+}
+
 const view_all_emps = (connection) => {
-    connection.query("SELECT employees.id AS id, employees.first_name AS first, employees.last_name AS last, employees.manager_id AS manager, roles.title AS role, departments.dept_name AS department FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.dept_id = departments.id;", (err, res) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.table(res, ["id", "first", "last", "role", "department", "manager"]);
-        }
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT employees.id AS id, employees.first_name AS first, employees.last_name AS last, employees.manager_id AS manager,
+        roles.title AS role, departments.dept_name AS department
+        FROM employees
+        INNER JOIN roles ON employees.role_id = roles.id
+        INNER JOIN departments ON roles.dept_id = departments.id`, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log();
+                console.table(res, ["id", "first", "last", "role", "department", "manager"]);
+            }
+            wait_to_resolve(resolve);
+        });
     });
     // connection.query(`SELECT employees.first_name AS first, employees.last_name AS last, roles.title AS role
     //                 FROM employees JOIN roles
