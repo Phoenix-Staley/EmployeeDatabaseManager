c64acbe11bddd42b171f22e5c4395357e2bd188d
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-13 12:53:17 -0700

    Modify seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,13 +3,14 @@ USE company_db;
 INSERT INTO departments (dept_name)
 VALUES ("Engineering"),
 ("Sales"),
-("Finance"),
-("Legal");
+("Finance");
 
 INSERT INTO roles (title, salary, dept_id)
 VALUES ("Engineer", 80000, 1),
 ("Manager", 100000, 1),
-("Intern", 60000, 2);
+("Intern", 60000, 1),
+("Salesperson", 75000, 2),
+("Accountant", 80000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Jordan", "Johnson", 2, NULL),
